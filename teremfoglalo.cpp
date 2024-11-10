f0bcd47c6d5ddeedaae8be4a27b0618946cb5757
Vilmos Cseke <90458352+sid14925@users.noreply.github.com>
2024-11-10 13:01:04 +0100

    Create teremfoglalo.cpp

---
+++ teremfoglalo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Foglalas {
+    string tanar_neve;
+    string tantargy;
+    int terem_szam;
+    int nap; // 1 - Hétfő, 2 - Kedd, ..., 7 - Vasárnap
+    int kezd_ido; // Kezdési időpont (óra 0-tól 23-ig)
+    int veg_ido;  // Befejezési időpont (óra 0-tól 23-ig)
+};
+
+
+int main() {
+
+}
